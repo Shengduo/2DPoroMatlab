45b4f5488aeaaf9c86b3948b8e7002a20f5d2dc8
Shengduo Liu <shengduoliu@ShengduodeMacBook-Pro.local>
2023-08-27 11:00:16 -0700

    staged 0827 MBP

---
+++ Regularized_study_script_new_0827.m
@@ -0,0 +1,21 @@
+clc,clear;
+close all;
+% Injection over one [hour]
+%delete(gcp('nocreate'));
+%parpool(1);
+
+% Dilatancy coefficient gamma varies as the array below
+gammas = [0];
+
+namePrefix = "WesterlyGranite";
+
+kpa_cxs = [8.7584e-11, 8.7584e-13];
+% No flash heating
+FHFlag = 0;
+
+
+for jjj = 1:1:size(gammas, 2)
+    for iii = 1:1:size(kpa_cxs, 2)
+        Regularized_cluster_new_0827(gammas(jjj), FHFlag, 3, kpa_cxs(iii), namePrefix);
+    end
+end
