f349508b91f3a10f7bd035cb7c3e6176f540c30a
Shengduo Liu <sliu5@hpc-82-33.cm.cluster>
2024-05-14 18:22:25 -0700

    shit.

--- Regularized_study_script_diffusion_factors_p_control_1_narrow.m
+++ Regularized_study_script_diffusion_factors_p_control_1_narrow.m
@@ -18,9 +18,9 @@ ccs = [1.0e-8];
 FHFlag = 0;
 
 % All diffusion factors
-kappacx_factors = [1.e-4];
-kappacy_factors = [1.];
-bulkc_factors = [1.e2];
+kappacx_factors = [1.e-4, 1.e-4, 1.e-4];
+kappacy_factors = [1., 1.e2, 1.e4];
+bulkc_factors = [1., 1.e2, 1.e4];
 
 % Injection flux
 baseFlux = 1.0e-4;
@@ -37,12 +37,12 @@ for iii = 1:1:size(nuus, 2)
     for jjj = 1:1:size(gammas, 2)
         for kkk = 1:1:size(ccs, 2)
             for lll = 1:1:size(kappacx_factors, 2)
-                for mmm = 1:1:size(kappacy_factors, 2)
-                    for nnn = 1:1:size(bulkc_factors, 2)
+                % for mmm = 1:1:size(kappacy_factors, 2)
+                    % for nnn = 1:1:size(bulkc_factors, 2)
                         for fff = 1:1:size(fluxes, 2)
                             factors = [kappacx_factors(lll), ...
-                                       kappacy_factors(mmm), ...
-                                       bulkc_factors(nnn)];
+                                       kappacy_factors(lll), ...
+                                       bulkc_factors(lll)];
                             Rc_diffusion_factors_p_control_elastic_1_narrow( nuus(iii), nus(iii), ...
                                 gammas(jjj), ccs(kkk), FHFlag, 3, factors, ...
                                 fluxes(fff), Elastic_Flag); 
@@ -50,8 +50,8 @@ for iii = 1:1:size(nuus, 2)
                                 %, ...
                                 % Terminating_times(iii));
                         end
-                    end
-                end
+                    % end
+                % end
             end
         end
     end
